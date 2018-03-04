2c05041742de2f1c152b2fa872e326ba7228c0dd
Unknown <wadamek65@gmail.com>
2018-03-04 19:33:02 +0100

    Organized files and functions. Create the base of frontend in flask.

--- backend/extractData.py
+++ backend/extractData.py
@@ -1,44 +1,35 @@
 from cassandra.cluster import Cluster
-import csv
+import sys               # These 2 lines allow us for better organization.
+sys.path.append('../')   # I moved function definition to another file since it needs to be also used elsewhere.
+from toolbox.kpiFileReader import kpi_get_collection_dictionary
 
-cassandraCluster = Cluster()
+cassandra_cluster = Cluster()
+session = cassandra_cluster.connect('pb2')
+query = session.execute('SELECT * FROM plmn_raw LIMIT 10000') # Gets all data from database
 
-session = cassandraCluster.connect('pb2')
-
-query = session.execute('SELECT * FROM plmn_raw LIMIT 1000000') # Gets all data from database
-
-kpiDictionary = {}
-kpiDataFile = 'kpi_collection_ready.csv'
-
-# This creates a dictionary for easy look-ups on units.
-# Key is the KPI name and it's value is array consisting of 3 values in this exact order: unit name, minimum value, maximum value
-with open(kpiDataFile, 'r') as row:
-    kpiReader = csv.reader(row)
-    next(kpiReader)
-    for readerRow in kpiReader:
-        kpiDictionary[readerRow[0]] = readerRow[1:4]
-
-#print(kpiDictionary)
+kpi_dictionary = kpi_get_collection_dictionary()
 
 # Function checks if the value is in correct range of the unit
 # @params a row of data from query (Can possibly be changed to raw kpi name and it's value)
 # @returns True or False depending if the value is correct
-faultyKeys = [] # This is for testing purposes to store the keys that are not in dictionary.
-def isValueInRange(queryRow):
+faulty_keys = [] # This is for testing purposes to store the keys that are not in dictionary.
+
+
+def is_value_in_range(query_row):
     try:
-        minValue = kpiDictionary[queryRow[4]][1] # Take the second value (minimum) from dictionary with 4th key which is kpi_name in the table
-        maxValue = kpiDictionary[queryRow[4]][2] # Same as above except third value which is minimum
-        if not compare(minValue, maxValue, queryRow[6]):
+        min_value = kpi_dictionary[query_row[4]][1] # Take the second value (minimum) from dictionary with 4th key which is kpi_name in the table
+        max_value = kpi_dictionary[query_row[4]][2] # Same as above except third value which is minimum
+        if not compare(min_value, max_value, query_row[6]):
             print('The value is not in range')
-            print(queryRow)
+            print(query_row)
     except TypeError:
         print('Type error')
-        print(queryRow)
-        print(minValue)
-        print(maxValue)
+        print(query_row)
+        print(min_value)
+        print(max_value)
     except KeyError:
-        if queryRow[4] not in faultyKeys:   # Check for keys not found in the kpi file
-            faultyKeys.append(queryRow[4])  # Append keys that are not found
+        if query_row[4] not in faulty_keys:   # Check for keys not found in the kpi file
+            faulty_keys.append(query_row[4])  # Append keys that are not found
         #print('Key error')
         #print(queryRow[4])
 
@@ -47,6 +38,8 @@ def isValueInRange(queryRow):
 # @params minimum value, maximum value and the real value
 # @returns True if real value is between min and max value, else False
 # THIS FUNCTION IS GARBAGE. CHANGE IT!!
+
+
 def compare(minVal, maxVal, realVal):
     if minVal == maxVal:
         return True
@@ -65,7 +58,8 @@ def compare(minVal, maxVal, realVal):
     else:
         return True
 
-for row in query:
-    isValueInRange(row)
 
-print(faultyKeys)
+for row in query:
+    is_value_in_range(row)
+
+print(faulty_keys)
