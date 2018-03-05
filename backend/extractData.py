d87a3334f51bb2692303afc7d8ca8759bfddd5db
Unknown <wadamek65@gmail.com>
2018-03-05 23:50:35 +0100

    Problem resolution. Further optimization and organization.

--- backend/extractData.py
+++ backend/extractData.py
@@ -1,12 +1,11 @@
 from cassandra.cluster import Cluster
-import sys               # These 2 lines allow us for better organization.
-from toolbox.kpiFileReader import kpi_get_collection_dictionary
 from difflib import get_close_matches
-sys.path.append('../')   # I moved function definition to another file since it needs to be also used elsewhere.
+import datetime
+from cassandra_utils import cassandra_get_unit_data
 
 
-kpi_dictionary = kpi_get_collection_dictionary()
-closest_words_dictionary = {}
+kpi_dictionary = cassandra_get_unit_data()
+print(kpi_dictionary)
 
 # Function checks if the value is in correct range of the unit
 # @params a row of data from query (Can possibly be changed to raw kpi name and it's value)
@@ -19,25 +18,28 @@ def is_value_in_range(query_row):
     min_value, max_value = get_values_from_dictionary(query_row[4], kpi_dictionary)
 
     # If returned values are false - kpi not found in the standard dictionary, try to find a close match.
-    if not min_value and not max_value:     # Name not found in the dictionary
-        min_value, max_value = get_values_from_dictionary(query_row[4], closest_words_dictionary)
-        if not min_value and not max_value:
+    if query_row[4] in faulty_keys:
+        return False
+    else:
+        if min_value is False and max_value is False:     # Name not found in the dictionary
+            #print(min_value, max_value, query_row[4])
             # Get 3 closest words with cutoff 0.9 (very similiar)
             # The function is slow so to not call it every time, we store the data in a new dictionary.
             # This can be changed to append the existing dictionary.
             close_words = get_close_matches(query_row[4], kpi_dictionary, 3, 0.8)
-            if len(close_words) != 0:   # Copies the values into new dictionary with other kpi names.
-                kpi_dictionary[query_row[4]] = [kpi_dictionary[close_words[0]][0],
-                                                kpi_dictionary[close_words[0]][1],
-                                                kpi_dictionary[close_words[0]][2]]
-                min_value, max_value = get_values_from_dictionary(query_row[4], kpi_dictionary)
+            if len(close_words) != 0:   # Similiar results found.
+                for close_word in close_words:  # Iterate through found similarities.
+                    if close_word in kpi_dictionary:    # If one of them is in dictionary, add them.
+                        kpi_dictionary[query_row[4]] = [kpi_dictionary[close_word][0],
+                                                        kpi_dictionary[close_word][1],
+                                                        kpi_dictionary[close_word][2],
+                                                        close_words[0]]
+                        min_value, max_value = get_values_from_dictionary(query_row[4], kpi_dictionary)
+                        break
             else:
                 if query_row[4] not in faulty_keys:
                     faulty_keys.append(query_row[4])
 
-    if not min_value and not max_value:
-        return False
-    else:
         return compare(min_value, max_value, query_row[6])
 
 
@@ -48,13 +50,11 @@ def compare(min_val, max_val, real_val):
     if not min_val and not max_val:
         return True
     elif not max_val:
-        min_val = float(min_val) # Need to convert it to float since all query data is string.
         if real_val >= min_val:
             return True
         else:
             return False
     elif not min_val:
-        max_val = float(max_val)
         if real_val <= max_val:
             return True
         else:
@@ -68,26 +68,28 @@ def compare(min_val, max_val, real_val):
 # @returns minimum and maximum value of unit range
 def get_values_from_dictionary(name, diction):
     if name in diction:
-        return diction[name][1], diction[name][2]
+        return diction[name][1], diction[name][0]
     elif name.upper() in diction:   # Check for both upper and lower letter cases.
-        return diction[name.upper()][1], diction[name.upper()][2]
+        return diction[name.upper()][1], diction[name.upper()][0]
     elif name.lower() in diction:
-        return diction[name.lower()][1], diction[name.lower()][2]
+        return diction[name.lower()][1], diction[name.lower()][0]
     else:
         return False, False
 
 
 cassandra_cluster = Cluster()
 session = cassandra_cluster.connect('pb2')
-#query = session.prepare('SELECT * FROM plmn_raw WHERE kpi_basename=? LIMIT 100000')
-#query_result = session.execute(query, ('SGSN_2012',)) # Gets all data from database
-query_result = session.execute('SELECT * FROM plmn_raw LIMIT 10000')
+query = session.prepare('SELECT * FROM plmn_raw_date WHERE date=?')
+new_date = datetime.datetime(2018, 2, 1)
+print(new_date)
+query_result = session.execute(query, (new_date,)) # Gets all data from database
 
-# THIS WILL BE CHANGED TO EXTRACT DATA IN PARTS USING ORDER BY DATE.
 for row in query_result:
     if not is_value_in_range(row) and row[4] not in faulty_keys:
         print(row)
-        print(faulty_keys)
+        print(row[4])
+        print(get_values_from_dictionary(row[4], kpi_dictionary))
+        #print(faulty_keys)
 
 print(faulty_keys)
 print(kpi_dictionary)
@@ -104,17 +106,12 @@ CREATE TABLE IF NOT EXISTS plmn_raw_date (
 ) WITH CLUSTERING ORDER BY (kpi_basename DESC) AND 
 COMPACTION={'class':'DateTieredCompactionStrategy', 'timestamp_resolution':'DAYS'};
   
-CREATE TABLE IF NOT EXISTS plmn_processed (
+CREATE TABLE IF NOT EXISTS kpi_units (
   kpi_name TEXT,
-  kpi_basename TEXT,
-  kpi_version TEXT,
-  cord_id BIGINT,
-  acronym TEXT,
-  date TIMESTAMP,
-  value DOUBLE,
- unit TEXT,
-  PRIMARY KEY (kpi_basename, date, cord_id, acronym, kpi_name)
-) WITH CLUSTERING ORDER BY (date DESC) AND
-  COMPACTION={'class':'DateTieredCompactionStrategy', 'timestamp_resolution':'DAYS'};
+  unit TEXT,
+  min DOUBLE,
+  max DOUBLE,
+  PRIMARY KEY (kpi_name)
+) WITH COMPACTION={'class':'DateTieredCompactionStrategy'};
 
 """
