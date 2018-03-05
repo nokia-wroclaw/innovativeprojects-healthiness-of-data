3523464afae57f3fd52501d17d9e8bd0ff87a2f7
Jakub Walecki <jwalecki1996@gmail.com>
2018-03-05 15:51:29 +0100

    some fixes, names refactor and basing access to returned value from cassandra

--- frontend/Flask/app/cassandraCommunication.py
+++ frontend/Flask/app/cassandraCommunication.py
@@ -30,39 +30,42 @@ def check_date(year, month, day):
         print('Bad date')
         return False
     except:
-        print('Bad date')
+        print('Unknown exception')
         return False
 
 
-def get_data_from_cassandra(startdatestring, enddatestring, name):
+def get_data_from_cassandra(start_date_string, end_date_string, name):
     if not check_name(name):
         return False
     else:
-        start_year, start_month, start_day = parse_date(startdatestring)
-        end_year, end_month, end_day = parse_date(enddatestring)
+        start_year, start_month, start_day = parse_date(start_date_string)
+        end_year, end_month, end_day = parse_date(end_date_string)
         start_date = check_date(start_year, start_month, start_day)
         end_date = check_date(end_year, end_month, end_day)
         if not start_date or not end_date:
             return False
         else:
-            try:
-                cassandra_cluster = Cluster()
-                session = cassandra_cluster.connect('pb2')
-
-                # THIS IS A WORKAROUND. NAME QUERY DOESN'T HAVE A KPI VERSION, JUST THE BASENAME.
-                # CALLING JUST SGSN_2012 WOULD RESULT IN FAILURE
-                # SO UNTIL WE COME UP WITH AN IDEA FOR THAT, WE WILL CALL FOR SGSN_2012A AND REMOVE LAST CHARACTER
-                name = name[:-1]
-                get_data = session.prepare('SELECT * FROM plmn_raw WHERE kpi_basename=? AND date > ? AND date < ?')
-                data = session.execute(get_data, (name, start_date, end_date,))
-                print('Success')
-                return data
-            except: # IMPROVE THIS
-                print('Exception')
-                return False
+            # try:
+            cassandra_cluster = Cluster()
+            session = cassandra_cluster.connect('pb2')
+            data_list =[]
+            # THIS IS A WORKAROUND. NAME QUERY DOESN'T HAVE A KPI VERSION, JUST THE BASENAME.
+            # CALLING JUST SGSN_2012 WOULD RESULT IN FAILURE
+            # SO UNTIL WE COME UP WITH AN IDEA FOR THAT, WE WILL CALL FOR SGSN_2012A AND REMOVE LAST CHARACTER
+            name = name[:-1]
+            get_data = session.prepare('SELECT * FROM plmn_raw WHERE kpi_basename=? AND date > ? AND date < ? LIMIT 1')
+            data = session.execute(get_data, (name, start_date, end_date,))
+            copy = data
+            for row in copy:
+                print(row)
+                # print(dir(row))
+                # print(row._fields)
+                for attribute in row._fields:
+                    print(attribute, row.__getattribute__(attribute))
+            print('Success')
+            return data
+            # except: # IMPROVE THIS
+            #     print('Exception')
+            #     return False
 
 #data = get_data_from_cassandra('2018-01-01', '2018-02-01', 'SGSN_2012a')
-
-
-
-
