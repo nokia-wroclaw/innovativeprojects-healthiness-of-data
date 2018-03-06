0083713c0d62eb55eb137117e2c0147689973d2a
Jakub Walecki <jwalecki1996@gmail.com>
2018-03-06 21:10:24 +0100

    sanitizig code

--- processing/process_data.py
+++ processing/process_data.py
@@ -1,30 +1,37 @@
-from cassandra.cluster import Cluster
+"""
+This module is the beginning of data processing module
+For now it can check if value is in range
+"""
 from difflib import get_close_matches
 import datetime
+from cassandra.cluster import Cluster
 from cassandra_utils import cassandra_get_unit_data
 
 
 kpi_dictionary = cassandra_get_unit_data()
 print(kpi_dictionary)
-
-# Function checks if the value is in correct range of the unit
-# @params a row of data from query (Can possibly be changed to raw kpi name and it's value)
-# @returns True or False depending if the value is correct
 faulty_keys = []    # This is for testing purposes to store the keys that are not in dictionary.
 
 
 def is_value_in_range(query_row):
+    """
+    function checks if value falls between logical bounds
+    :param query_row: row of db to be checked
+    :return: bool value whether it is ok or not
+    """
     # Take the range values from the dictionary with 4th key which is kpi_name in the table
     min_value, max_value = get_values_from_dictionary(query_row[4], kpi_dictionary)
 
-    # If returned values are false - kpi not found in the standard dictionary, try to find a close match.
+    # If returned values are false - kpi not found in the standard dictionary,
+    # try to find a close match.
     if query_row[4] in faulty_keys:
         return False
     else:
         if min_value is False and max_value is False:     # Name not found in the dictionary
             #print(min_value, max_value, query_row[4])
             # Get 3 closest words with cutoff 0.9 (very similiar)
-            # The function is slow so to not call it every time, we store the data in a new dictionary.
+            # The function is slow so to not call it every time,
+            # we store the data in a new dictionary.
             # This can be changed to append the existing dictionary.
             close_words = get_close_matches(query_row[4], kpi_dictionary, 3, 0.8)
             if len(close_words) != 0:   # Similiar results found.
@@ -34,7 +41,9 @@ def is_value_in_range(query_row):
                                                         kpi_dictionary[close_word][1],
                                                         kpi_dictionary[close_word][2],
                                                         close_words[0]]
-                        min_value, max_value = get_values_from_dictionary(query_row[4], kpi_dictionary)
+                        min_value, max_value = get_values_from_dictionary(
+                            query_row[4],
+                            kpi_dictionary)
                         break
             else:
                 if query_row[4] not in faulty_keys:
@@ -43,10 +52,14 @@ def is_value_in_range(query_row):
         return compare(min_value, max_value, query_row[6])
 
 
-# This function could be further optimized
-# @params minimum value, maximum value and the real value
-# @returns True if real value is between min and max value, else False
 def compare(min_val, max_val, real_val):
+    """
+    bounds checker
+    :param min_val:
+    :param max_val:
+    :param real_val:
+    :return:
+    """
     if not min_val and not max_val:
         return True
     elif not max_val:
@@ -63,10 +76,13 @@ def compare(min_val, max_val, real_val):
         return False
 
 
-# Looks up the unit range in the given dictionary. Either the standard kpi or the new closest one.
-# @params kpi_name and dictionary in which to look up
-# @returns minimum and maximum value of unit range
 def get_values_from_dictionary(name, diction):
+    """
+    dictionary reader
+    :param name:
+    :param diction:
+    :return:
+    """
     if name in diction:
         return diction[name][1], diction[name][0]
     elif name.upper() in diction:   # Check for both upper and lower letter cases.
@@ -79,9 +95,9 @@ def get_values_from_dictionary(name, diction):
 
 cassandra_cluster = Cluster()
 session = cassandra_cluster.connect('pb2')
-query = session.prepare('SELECT * FROM plmn_raw_date WHERE date=?') # This will be replaced with an object mapper in the future.
+# This will be replaced with an object mapper in the future.
+query = session.prepare('SELECT * FROM plmn_raw_date WHERE date=?')
 new_date = datetime.datetime(2018, 2, 1) # select all entries from a day.
-print(new_date)
 query_result = session.execute(query, (new_date,)) # Gets all data from database
 
 # IF THE VALUE IS FAULTY PRINT IT.
