d52f017578c198d3496777e8e45af4536caa1f8a
Unknown <wadamek65@gmail.com>
2018-03-06 17:51:01 +0100

    Little improvements to flask app functionality.

--- backend/Flask/app/cassandraCommunication.py
+++ backend/Flask/app/cassandraCommunication.py
@@ -1,13 +1,16 @@
 from cassandra.cluster import Cluster
 import sys
 sys.path.append(sys.path[0] + "/../../")
-from toolbox.kpiFileReader import kpi_get_collection_dictionary
+from processing.cassandra_utils import cassandra_get_unit_data
 import datetime
+
+# Modify this if you want more data passed to the template.
+desired_keys = ['kpi_basename', 'acronym', 'value', 'date']
 
 
 # Check if the URL name is in the KPI dictionary
 def check_name(name):
-    dictionary = kpi_get_collection_dictionary()
+    dictionary = cassandra_get_unit_data()
     if name.lower() not in dictionary and name.upper() not in dictionary:
         print('Wrong name')
         return False
@@ -15,13 +18,13 @@ def check_name(name):
         return True
 
 
-# Parse the date given in URL in format dd-mm-yyyy
+# Parse the date given in URL in format yyyy-mm-dd
 def parse_date(entry):
     year, month, date = map(int, entry.split('-'))
     return year, month, date
 
 
-# Check if the date given is real
+# Check if the date given is correct
 def check_date(year, month, day):
     try:
         new_date = datetime.datetime(year, month, day)
@@ -30,39 +33,44 @@ def check_date(year, month, day):
         print('Bad date')
         return False
     except:
-        print('Bad date')
+        print('Unknown exception')
         return False
 
 
-def get_data_from_cassandra(startdatestring, enddatestring, name):
+def get_data_from_cassandra(start_date_string, end_date_string, name):
     if not check_name(name):
         return False
     else:
-        start_year, start_month, start_day = parse_date(startdatestring)
-        end_year, end_month, end_day = parse_date(enddatestring)
+        start_year, start_month, start_day = parse_date(start_date_string)
+        end_year, end_month, end_day = parse_date(end_date_string)
         start_date = check_date(start_year, start_month, start_day)
         end_date = check_date(end_year, end_month, end_day)
         if not start_date or not end_date:
             return False
         else:
-            try:
-                cassandra_cluster = Cluster()
-                session = cassandra_cluster.connect('pb2')
-
-                # THIS IS A WORKAROUND. NAME QUERY DOESN'T HAVE A KPI VERSION, JUST THE BASENAME.
-                # CALLING JUST SGSN_2012 WOULD RESULT IN FAILURE
-                # SO UNTIL WE COME UP WITH AN IDEA FOR THAT, WE WILL CALL FOR SGSN_2012A AND REMOVE LAST CHARACTER
-                name = name[:-1]
-                get_data = session.prepare('SELECT * FROM plmn_raw WHERE kpi_basename=? AND date > ? AND date < ?')
-                data = session.execute(get_data, (name, start_date, end_date,))
-                print('Success')
-                return data
-            except: # IMPROVE THIS
-                print('Exception')
-                return False
-
-#data = get_data_from_cassandra('2018-01-01', '2018-02-01', 'SGSN_2012a')
-
-
-
-
+            # try:
+            cassandra_cluster = Cluster()
+            session = cassandra_cluster.connect('pb2')
+            data_list = []
+            # THIS IS A WORKAROUND. NAME QUERY DOESN'T HAVE A KPI VERSION, JUST THE BASENAME.
+            # CALLING JUST SGSN_2012 WOULD RESULT IN FAILURE
+            # SO UNTIL WE COME UP WITH AN IDEA FOR THAT, WE WILL CALL FOR SGSN_2012A AND REMOVE LAST CHARACTER
+            name = name[:-1]
+            get_data = session.prepare('SELECT * FROM plmn_raw WHERE kpi_basename=? AND date > ? AND date < ?')
+            data = session.execute(get_data, (name, start_date, end_date,))
+            copy = data
+            for row in copy:
+                # print(row)
+                tmp = {}
+                # print(dir(row))
+                # print(row._fields)
+                for attribute in row._fields:
+                    if attribute in desired_keys:
+                        tmp[attribute] = row.__getattribute__(attribute)
+                data_list.append(tmp)
+            print('Success')
+            coverage = 0
+            for row in data_list:
+                coverage += row['value']
+            coverage = coverage / len(data_list)
+            return data_list, coverage
