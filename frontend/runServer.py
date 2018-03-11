c0e1eccf97fac2d7106298cb506496af44b02170
Dominika Maślanka <dominika.maslanka@outlook.com>
2018-03-11 12:41:30 +0100

    Importing cassandra added, could someone check it?

--- frontend/runServer.py
+++ frontend/runServer.py
@@ -1,7 +1,15 @@
 print "Great job"
 
 import cassandra
+import cqlsh
+con = cql.connect('127.0.0.1', 9042,  keyspace, cqlsh_version='TUTAJ_NIE_WIEM ')
+print("Connect test - Great job!")
+cursor = con.cursor()
+CQLString = "INSERT INTO PRIMATY KEY (kpi_basename, date, cord_id, acronym, kpi_name);"
+cursor.execute(CQLString)
+
 from app import app
+
 # RUN THIS SCRIPT OR THE FOLLOWING COMMANDS:
 # COMMAND HAS TO BE EXECUTED FROM THE DIRECTORY OF THIS FILE.
 # LINUX: export FLASK_APP=coverage.py
