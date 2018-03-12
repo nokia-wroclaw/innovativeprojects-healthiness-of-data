5b8a4e50ceeda865730603fbb73973d8a8fe0240
Jakub Walecki <jwalecki1996@gmail.com>
2018-03-12 09:09:22 +0100

    fix in envs for machine provisioning and remote testing

--- CI/run_instance.py
+++ CI/run_instance.py
@@ -1,7 +1,7 @@
 import os
 from digitalocean import Manager, Droplet
 from time import sleep
-
+from time import time
 
 connection = Manager(token=os.environ["DO_TOKEN"])
 key = []
@@ -12,14 +12,18 @@ for i in connection.get_my_images():
         break
 srv = Droplet(token=os.environ["DO_TOKEN"], image=image, size="s-1vcpu-1gb", region="fra1", ssh_keys=key, \
               name=os.environ["TRAVIS_COMMIT"])
+start = time()
 srv.create()
 for i in range(1,12):
-    print("Waitnig for droplet configuration; {} passed".format(i*10))
     if srv.get_actions()[0].status == 'completed':
+        stop = time()
+        print("Instance provisioninng took {}. Saving envs to run_instance_envs.".format(stop-start))
         ip_addr = Droplet.get_object(api_token=os.environ["DO_TOKEN"], droplet_id=srv.id).ip_address
         print(ip_addr)
+        f=open("run_instance_envs", 'w')
+        f.write("export DROPLET_IP={}".format(ip_addr))
+        f.close()
         sleep(10)
-        os.putenv("DROPLET_IP", ip_addr)
         exit(0)
     else:
         sleep(10)
