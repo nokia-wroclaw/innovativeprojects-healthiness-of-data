caad970849fa9b5f692020b3551a5ded852d0cf2
Jakub Walecki <jwalecki1996@gmail.com>
2018-03-12 21:12:00 +0100

    amends in ci machine creation

--- CI/run_instance.py
+++ CI/run_instance.py
@@ -5,13 +5,13 @@ from time import time
 
 connection = Manager(token=os.environ["DO_TOKEN"])
 key = []
-key.append(connection.get_ssh_key("19070495"))
+key.append(connection.get_ssh_key("18830428"))
 for i in connection.get_my_images():
     if i.name == os.environ["IMAGE_NAME"]:
         image = connection.get_image(i.id).id
         break
 srv = Droplet(token=os.environ["DO_TOKEN"], image=image, size="s-1vcpu-1gb", region="fra1", ssh_keys=key, \
-              name=os.environ["TRAVIS_COMMIT"])
+              name=os.environ["TRAVIS_COMMIT"], user_data="#!/bin/bash\nsed -i 's%PasswordAuthentication no%PasswordAuthentication yes%g' /etc/ssh/sshd_config\nservice sshd restart\n")
 start = time()
 srv.create()
 for i in range(1,12):
