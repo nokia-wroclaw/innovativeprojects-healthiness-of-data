8a55d18808f33d79258adb932e1515d015f9b0d2
wacuuu <jwalecki1996@gmail.com>
2018-03-12 14:14:36 +0100

    ci edition. login on droplet

--- CI/execute_tests.py
+++ CI/execute_tests.py
@@ -1,3 +1,11 @@
 import os
+import paramiko
 
 print("Test execution on {}".format(os.environ["DROPLET_IP"]))
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(os.environ["DROPLET_IP"], username="ci", password=os.environ["SSH_PASSWORD"], look_for_keys=False)
+stdin, stdout, stderr = client.exec_command('ls -la')
+for i in stdout.read().splitlines():
+    print(i)
