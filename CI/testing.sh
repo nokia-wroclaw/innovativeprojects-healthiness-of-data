908cc917b538e8eadb45750af62ae30b0ba22625
Jakub Walecki <jwalecki1996@gmail.com>
2018-03-16 23:23:24 +0100

    further CI improvements

--- CI/testing.sh
+++ CI/testing.sh
@@ -3,3 +3,6 @@ cd innovativeprojects-healthiness-of-data/
 echo "Run pylint"
 for i in $(find . -name '*py'); do pylint $i|tee -a pylint.log; done
 cat pylint.log|grep 'Your code has been rated at'|awk '{print $7}'|cut -f1 -d'/'|tr '\n' ' '|awk '{s+=$1}END{print "Average pylint score:",s/NR}' RS=" "
+cd ~/innovativeprojects-healthiness-of-data/frontend/frontend-angular
+ng test --browser ChromeHeadless 2>&1|tee -a ngTest.log
+
