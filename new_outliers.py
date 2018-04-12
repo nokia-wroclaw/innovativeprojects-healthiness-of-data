803a81f6dcd2d14422f8958023c1cbca13dd86f3
Unknown <wadamek65@gmail.com>
2018-04-13 01:21:53 +0200

    outliers new implementation

--- new_outliers.py
+++ new_outliers.py
@@ -6,7 +6,7 @@ from toolbox.cassandra_object_mapper_models import PlmnProcessedCord
 from matplotlib import pyplot
 import math
 
-
+window_size = 80
 def find_outliers(start_date, end_date, kpi_basename, cord_id, acronym, threshold):
     """
     Function for finding outliers.
@@ -40,7 +40,7 @@ def find_outliers(start_date, end_date, kpi_basename, cord_id, acronym, threshol
                 ready_data["values"].append(row.value)
                 ready_data["dates"].append(row.date)
 
-        window_size = 30
+
 
         data_right = []
         data_left = []
@@ -62,43 +62,68 @@ def find_outliers(start_date, end_date, kpi_basename, cord_id, acronym, threshol
             temp_data += [temp_data[0]] * window_size
 
         moving_mean = []
-        for i in range(0, len(ready_data['values'])):
-            moving_mean.append(numpy.median(temp_data[i:i + window_size]))
+        moving_standard_deviation = []
+        for i in range(window_size, len(ready_data['values'])-window_size):
+            temp_mean1 = numpy.median(ready_data['values'][i - window_size: i])
+            temp_mean2 = numpy.median(ready_data['values'][i - window_size//2: i + window_size//2])
+            temp_mean3 = numpy.median(ready_data['values'][i: i + window_size])
+            temp_mean_array = [temp_mean1, temp_mean2, temp_mean3]
+
+            closest_mean_index = (numpy.abs(numpy.array(temp_mean_array)-ready_data['values'][i])).argmin()
+            moving_mean.append(temp_mean_array[closest_mean_index])
+
+            moving_standard_deviation.append(numpy.std(ready_data['values'][i - window_size: i + window_size]))
+
+        # new_array = []
+        # for i in range(0, window_size):
+        #     new_array.append(numpy.mean(ready_data['values'][i: i + window_size]))
+        #     moving_standard_deviation.append(numpy.std(ready_data['values'][i: i + window_size]))
+        #
+        # for i in range(len(ready_data['values'])-window_size, len(ready_data['values'])):
+        #     moving_mean.append(numpy.mean(ready_data['values'][i-window_size: i]))
+            #moving_standard_deviation.append(numpy.std(ready_data['values'][i: i - window_size]))
+
+        #moving_mean = new_array + moving_mean
+
 
         if not threshold:
             threshold = 3.5
-
-
-
 
         median = numpy.median(ready_data["values"])
         mad_values = []
         modified_z_scores = []
         x = 0
-        for val in ready_data["values"]:
-            mad_values.append(numpy.abs(val - median))
-        median_absolute_deviation = numpy.median(mad_values)
-        for val in ready_data["values"]:
-            modified_z_scores.append(0.6745 * (val - moving_mean[x]) / median_absolute_deviation)
-            x += 1
 
+        # for val in ready_data["values"]:
+        #     mad_values.append(numpy.abs(val - median))
+        # median_absolute_deviation = numpy.median(mad_values)
+        # for val in ready_data["values"]:
+        #     modified_z_scores.append(0.6745 * (val - moving_mean[x]) / median_absolute_deviation)
+        #     x += 1
+        for i in range(window_size, len(ready_data['values']) - window_size):
+            z_score = (ready_data['values'][i] - moving_mean[x]) / moving_standard_deviation[x]
+            modified_z_scores.append(z_score)
+            x+=1
 
 
 
         ready_data["outliers"] = numpy.where(numpy.abs(modified_z_scores) > threshold)[0].tolist()
+        ready_data['outliers'] = [elem + window_size for elem in ready_data['outliers']]
 
         variance_changes = {'values': [], 'dates': []}
-        for i in range(0, len(ready_data["values"]) - 41):
-            var1 = numpy.var(ready_data["values"][i:i + 20], ddof=1)
-            var2 = numpy.var(ready_data["values"][i + 18:i + 38], ddof=1)
-            factor = (math.log(max(var1,var2))**2) / (math.log(min(var1,var2))**2)
-            variance_changes['values'].append(factor*10+100)
-            variance_changes['dates'].append(ready_data['dates'][i])
+        # for i in range(0, len(ready_data["values"]) - 41):
+        #     var1 = numpy.var(ready_data["values"][i:i + 20], ddof=1)
+        #     var2 = numpy.var(ready_data["values"][i + 18:i + 38], ddof=1)
+        #     factor = (math.log(max(var1,var2))**2) / (math.log(min(var1,var2))**2)
+        #     variance_changes['values'].append(factor*10+100)
+        #     variance_changes['dates'].append(ready_data['dates'][i])
 
         for outlier in ready_data["outliers"]:
             ready_data["outlier_values"].append(ready_data["values"][outlier])
             ready_data["outlier_dates"].append(ready_data["dates"][outlier])
 
+        print(len(ready_data['values']))
+        print(len(moving_mean))
         return ready_data, moving_mean, variance_changes
 
 
@@ -107,15 +132,16 @@ ready_data, moving_mean, data_changes = find_outliers('2015-01-03', '2018-04-01'
 fig = pyplot.figure()
 ax = fig.add_subplot(211)
 ax.plot(ready_data['dates'], ready_data['values'], marker='o', linestyle='None', markersize=2)
-ax.plot(ready_data['dates'], moving_mean, color='r')
+ax.plot(ready_data['dates'][window_size:-window_size], moving_mean, color='r')
 ax.plot(ready_data["outlier_dates"], ready_data["outlier_values"], marker='x', linestyle='None', markersize=8)
-ax.plot(data_changes['dates'], data_changes['values'], marker='o', linestyle='--', color='g', markersize=1)
+#ax.plot(data_changes['dates'], data_changes['values'], marker='o', linestyle='--', color='g', markersize=1)
 
 ready_data, moving_mean, data_changes = find_outliers('2016-01-03', '2018-04-01', 'SGSN_2012', 'Skuntank', 'dilfihess', 3.5)
 ax2 = fig.add_subplot(212)
 ax2.plot(ready_data['dates'], ready_data['values'], marker='o', linestyle='None', markersize=2)
-ax2.plot(ready_data['dates'], moving_mean, color='r')
+ax2.plot(ready_data['dates'][window_size:-window_size], moving_mean, color='r')
 ax2.plot(ready_data["outlier_dates"], ready_data["outlier_values"], marker='x', linestyle='None', markersize=8)
-ax2.plot(data_changes['dates'], data_changes['values'], marker='o', linestyle='--', color='g', markersize=1)
+#ax2.plot(data_changes['dates'], data_changes['values'], marker='o', linestyle='--', color='g', markersize=1)
 
+fig.suptitle('Window size: %s' % window_size)
 pyplot.show()
